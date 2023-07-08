cd791ef3ef1fb4e473992e9176dd7e32c7ddc249
nikachu404 <yasinskaa.veronika@gmail.com>
2023-07-08 13:47:50 +0300

    initial setup

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
